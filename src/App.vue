8b8bf0f7fb309458bacb73f32990c9746b215291
unknown <>
2017-02-16 10:36:43 +0800

    'vue-to-do-list'

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <h2>TodoList</h2>
+    <input class="addInput" type="text" name="" v-model="newItem" v-on:keydown.enter="addItem">
+    <ul>
+      <todo v-for="(label, index) in items" :label=label :index=index @delete="deleteItem"> </todo>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Todo from 'components/Todo'
+export default {
+  name: 'app',
+  data () {
+    return {
+      newItem: '',
+      items: ['aaa']
+    }
+  },
+  components: {
+    Todo
+  },
+  methods: {
+    addItem: function () {
+      this.items.push(this.newItem)
+      this.newItem = ''
+    },
+    deleteItem: function (index) {
+      this.items.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 700px;
+  margin: 30px auto;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+.addInput {
+  width: 600px;
+  height: 36px;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,60 @@
+<template>
+  <li class="todo" @mouseenter="showDeleteTag" @mouseleave="hideDeleteTag">
+      <input type="checkbox" @click="itemCheck(item)" >
+      <p class="item-label" :class="{'line-through': checked }">{{index+1}}. {{label}}</p>
+      <p class="item-label" v-if="isShowDel" @click="delClick">del</p>   
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data () {
+    return {
+      isShowDel: false,
+      checked: false
+    }
+  },
+  props: {
+    label: '',
+    index: ''
+  },
+  methods: {
+    itemCheck: function (item) {
+      this.checked = !this.checked
+    },
+    showDeleteTag: function () {
+      this.isShowDel = true
+    },
+    hideDeleteTag: function () {
+      this.isShowDel = false
+    },
+    delClick: function () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+li {
+  list-style: none;
+  margin: 0 10px;
+  height: 36px;
+  line-height: 36px;
+}
+a {
+  color: #42b983;
+}
+.item-label {
+  display: inline;
+}
+.line-through {
+  text-decoration: line-through;
+}
+</style>
